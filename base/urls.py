722bc0aa0fa0adba4d35c0065bebd2fe2583a6a9
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-01-16 21:05:40 +0600

    initialized

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('auth_control.urls')),
+    path('api/user/', include('user_control.urls')),
+]
