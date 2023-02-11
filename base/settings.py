a90de6d27538bc6c0fe03a63e59149a5835e67f4
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-02-11 17:06:16 +0600

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ux3=k&$cyj4wr2ed1rfo%b5bm(z_8-lftc(hpo+h@#owwi+77*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -158,8 +158,8 @@ STATIC_URL = 'static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gktournament64@gmail.com'
-EMAIL_HOST_PASSWORD = 'lpqgkheqryicxppi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
