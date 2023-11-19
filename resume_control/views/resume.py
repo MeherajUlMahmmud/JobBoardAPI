23f7a9832320001af89562bad3d2492df711778d
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-11-19 22:39:43 +0600

    Update resume.py

--- resume_control/views/resume.py
+++ resume_control/views/resume.py
@@ -1,10 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
 
-from common.custom_pagination import CustomPageNumberPagination
+from common.custom_view import (
+    CustomCreateAPIView, CustomRetrieveAPIView, CustomListAPIView,
+)
 from resume_control.custom_filters import ResumeModelFilter
 from resume_control.models import ResumeModel, PersonalModel, ContactModel
 from resume_control.serializers.contact import ContactModelSerializer
@@ -13,49 +13,60 @@ from resume_control.serializers.resume import ResumeModelSerializer
 from user_control.models import ApplicantModel
 
 
-class ResumeModelViewSet(ModelViewSet):
-    http_method_names = [
-        'get', 'head', 'options',
-        'post', 'put', 'patch', 'delete',
-    ]
-    queryset = ResumeModel.objects.all().order_by('-created_at')
-    permission_classes = [IsAuthenticated]
-    pagination_class = CustomPageNumberPagination
+class GetResumeListAPIView(CustomListAPIView):
+    queryset = ResumeModel.objects.filter(is_active=True, is_deleted=False)
+    serializer_class = ResumeModelSerializer.List
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ['name']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
     filterset_class = ResumeModelFilter
 
-    def get_serializer_class(self):
-        if self.request.method == 'POST' or self.request.method == 'PUT' or self.request.method == 'PATCH':
-            return ResumeModelSerializer.Write
-        return ResumeModelSerializer.List
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_authenticated:
+            return queryset.filter(user=self.request.user)
+        return queryset
 
-    def create(self, request, *args, **kwargs):
-        serializer = ResumeModelSerializer.Write(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        resume = serializer.save()
-        print(resume)
+
+class GetResumeDetailsAPIView(CustomRetrieveAPIView):
+    queryset = ResumeModel.objects.filter(is_active=True, is_deleted=False)
+    serializer_class = ResumeModelSerializer.List
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if not request.user.check_object_permissions(request, instance):
+            return Response({'detail': 'You do not have permission to perform this action.'}, status=403)
+
+        resume_serializer_data = ResumeModelSerializer.List(instance).data
+
+        personal = PersonalModel.objects.filter(resume=instance).first()
+        personal_serializer_data = PersonalModelSerializer(personal).data
+        resume_serializer_data['personal'] = personal_serializer_data
+
+        contact = ContactModel.objects.filter(resume=instance).first()
+        contact_serializer_data = ContactModelSerializer(contact).data
+        resume_serializer_data['contact'] = contact_serializer_data
+
+        return Response(resume_serializer_data)
+
+
+class CreateResumeAPIView(CustomCreateAPIView):
+    serializer_class = ResumeModelSerializer.Write
+    queryset = ResumeModel.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        resume = serializer.save(created_by=self.request.user)
         applicant = ApplicantModel.objects.get(user=resume.user)
         PersonalModel.objects.create(
             resume=resume,
             first_name=applicant.first_name,
             last_name=applicant.last_name,
+            created_by=self.request.user,
         )
-        ContactModel.objects.create(resume=resume, email=resume.user.email)
+        ContactModel.objects.create(
+            resume=resume,
+            email=resume.user.email,
+            created_by=self.request.user,
+        )
         return Response(serializer.data, status=201)
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        resume = ResumeModel.objects.filter(uuid=instance.uuid).first()
-
-        resume_serializer_data = ResumeModelSerializer.List(resume).data
-
-        personal = PersonalModel.objects.filter(resume=resume).first()
-        personal_serializer_data = PersonalModelSerializer(personal).data
-        resume_serializer_data['personal'] = personal_serializer_data
-
-        contact = ContactModel.objects.filter(resume=resume.uuid).first()
-        contact_serializer_data = ContactModelSerializer(contact).data
-        resume_serializer_data['contact'] = contact_serializer_data
-
-        return Response(resume_serializer_data)
