8ec6d27137038d4d8f04bae7d677aef14e9b59ec
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-11-20 10:46:42 +0600

    certification

--- resume_control/serializers/certification.py
+++ resume_control/serializers/certification.py
@@ -1,19 +1,43 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, CharField, ValidationError
 
 from resume_control.models import *
+from user_control.serializers.user import UserModelSerializer
 
 
-class CertificationModelSerializerMeta(serializers.ModelSerializer):
+class CertificationModelSerializerMeta(ModelSerializer):
     class Meta:
         model = CertificationModel
-        fields = ('uuid', 'user', 'title', 'description', 'link', 'is_active')
+        fields = [
+            'user',
+            'title',
+            'description',
+            'link',
+            'start_date',
+            'end_date',
+        ]
 
 
 class CertificationModelSerializer:
     class List(CertificationModelSerializerMeta):
+        created_by = UserModelSerializer.Lite(read_only=True)
+        updated_by = UserModelSerializer.Lite(read_only=True)
+
+        class Meta(CertificationModelSerializerMeta.Meta):
+            fields = CertificationModelSerializerMeta.Meta.fields + [
+                'id',
+                'created_by',
+                'created_at',
+                'updated_by',
+                'updated_at',
+            ]
+
+    class Write(CertificationModelSerializerMeta):
+        title = CharField(max_length=255, required=True)
+
         class Meta(CertificationModelSerializerMeta.Meta):
             fields = CertificationModelSerializerMeta.Meta.fields
 
-    class Write(CertificationModelSerializerMeta):
-        class Meta(CertificationModelSerializerMeta.Meta):
-            fields = CertificationModelSerializerMeta.Meta.fields
+        def validate(self, attrs):
+            if attrs['resume'].user.id != self.context['request'].user.id:
+                raise ValidationError('You are not allowed to create award for this resume.')
+            return attrs
