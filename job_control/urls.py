fab5df0fef1d7f3f2160ceb81a31415b123dcbcd
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-11-22 16:33:36 +0600

    Update urls.py

--- job_control/urls.py
+++ job_control/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('job-type/create/', CreateJobTypeAPIView.as_view(), name='create_job_type'),
     path('job-type/<str:pk>/details/', GetJobTypeDetailsAPIView.as_view(), name='get_job_type_details'),
     path('job-type/<str:pk>/update/', UpdateJobTypeDetailsAPIView.as_view(), name='update_job_type_details'),
-
 ]
